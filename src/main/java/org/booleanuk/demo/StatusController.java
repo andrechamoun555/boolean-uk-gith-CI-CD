9612dd77a1e7c4edee1286003372e4407ccc44e2
andrechamoun555 <andre.chamoun03@gmail.com>
2025-09-04 13:00:33 +0200

    feat: add status enpoint with tests - Add GET method for api/status endpoint returning message - Add test for status endpoint

---
+++ src/main/java/org/booleanuk/demo/StatusController.java
@@ -0,0 +1,12 @@
+package org.booleanuk.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+    @GetMapping("/api/status")
+    public String status() {
+        return "Service is active";
+    }
+}
---
+++ src/test/java/org/booleanuk/demo/StatusControllerTest.java
@@ -0,0 +1,20 @@
+package org.booleanuk.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(StatusController.class)
+public class StatusControllerTest {
+
+    private StatusController statusController;
+
+    public StatusControllerTest() {
+        statusController = new StatusController();
+    }
+
+    @Test
+    void statusControllerReturn() throws Exception {
+        Assertions.assertEquals("Service is active", statusController.status());
+    }
+}
