f499fb43475c371c72c4e0840af028f25c4588b2
andrechamoun555 <andre.chamoun03@gmail.com>
2025-09-04 12:50:52 +0200

    init

---
+++ src/main/java/org/booleanuk/demo/BooleanUkGitactionCiCdApplication.java
@@ -0,0 +1,13 @@
+package org.booleanuk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooleanUkGitactionCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooleanUkGitactionCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/booleanuk/demo/BooleanUkGitactionCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.booleanuk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooleanUkGitactionCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
